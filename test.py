8cb65ac769223a4caab75a4eb4c1f22ea2c65392
Mrpsousa <urameshi.uba.com>
2019-01-03 12:02:37 -0300

    rede teste finalizada

--- test.py
+++ test.py
@@ -10,12 +10,18 @@ def sigmoidDerivada(sig):
 
 base = datasets.load_breast_cancer()
 entradas = base.data
-valoresSaidas =  base.target
+valoresSaidas =  base.target #aqui ta 1 linha e varias colunas, preciso inverter
+saidas = np.empty([569,1], dtype = int) #569 linhas por 1 coluna (temos 569 registros), vazio
+for i in range(569): #invertendo
+     saidas[i] = valoresSaidas[i]
 
-pesos0 = 2 * np.random.random((2,3)) -1 
-pesos1 = 2 * np.random.random((3,1)) -1 
 
-epocas = 1
+pesos0 = 2 * np.random.random((30,3)) -1 #como eu tenho 30 atributos de entrada, preciso de 30 pesos 
+                                         # e temos 3 neuronios na camada escondida, dae fica 30, 3, temos 1 camada oculta apenas com 3 neuronios
+pesos1 = 2 * np.random.random((3,1)) -1  #(3,1) 3 pesos da camada escondida e finaliza com 1 neuronio na camada saida
+
+
+epocas = 50000
 taxaAprendizagem = 0.6
 momento = 1
 
@@ -49,3 +55,4 @@ for j in range(epocas):
      pesos0 = (pesos0 * momento) + (pesosNovo0 * taxaAprendizagem)
 
 
+#temos 37% de erro, logo a rede tem 62% de acerto(fora os "quebrados")
