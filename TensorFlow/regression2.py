11f36986d8881bce610ae678014957de4b233020
Mrpsousa <urameshi.uba.com>
2019-01-24 17:00:48 -0300

    em andamento com TF

--- TensorFlow/regression2.py
+++ TensorFlow/regression2.py
@@ -3,6 +3,7 @@ import pandas as pd
 base = pd.read_csv('house-prices.csv')
 import tensorflow as tf 
 from sklearn.preprocessing import StandardScaler
+import numpy as np 
 
 #print(base.head())
 #print(base.count())
@@ -26,4 +27,19 @@ x = scaler_x.fit_transform(x)
 scaler_y = StandardScaler()
 y = scaler_x.fit_transform(y)
 
-print(x)
+#print(x)
+#import matplotlib.pylab as plt  
+#plt.scatter(x, y)
+ 
+#simple linear regression formula
+#y = b0 + b1 * x
+np.random.seed(0)
+#print(np.random.rand(2)) # to generate 2 random numbers
+np.random.rand(2)
+#this "random.rand" did generate two values, ([0.417022],[0.72032449])
+#creating variables
+b0 = tf.Variable(0.41)
+b1 = tf.Variable(0.72)
+
+#creating a placeholder, to receiver data of variables Y and X
+xph = tf.placeholder(tf.float32, [21613, 1]) # tf.float32 type of date, [a,b]   
