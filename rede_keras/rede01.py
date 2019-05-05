c6624d2d5ca15ffac760d790fde072fdde4a176e
Mrpsousa <urameshi.uba@gmail.com>
2019-05-05 20:47:53 -0300

    network working well

--- rede_keras/rede01.py
+++ rede_keras/rede01.py
@@ -125,9 +125,9 @@ NN_model = Sequential()
 NN_model.add(Dense(20, kernel_initializer='normal',input_dim = train.shape[1], activation='relu'))
 
 # The Hidden Layers :
-NN_model.add(Dense(36, kernel_initializer='normal',activation='relu'))
-NN_model.add(Dense(36, kernel_initializer='normal',activation='relu'))
-NN_model.add(Dense(36, kernel_initializer='normal',activation='relu'))
+NN_model.add(Dense(40, kernel_initializer='normal',activation='relu'))
+NN_model.add(Dense(40, kernel_initializer='normal',activation='relu'))
+NN_model.add(Dense(40, kernel_initializer='normal',activation='relu'))
 
 # The Output Layer :
 NN_model.add(Dense(1, kernel_initializer='normal',activation='linear'))
@@ -136,16 +136,18 @@ NN_model.add(Dense(1, kernel_initializer='normal',activation='linear'))
 NN_model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_absolute_error'])
 NN_model.summary()
 
+
 #Define a checkpoint callback
 checkpoint_name = 'Weights-{epoch:03d}--{val_loss:.5f}.hdf5' 
 checkpoint = ModelCheckpoint(checkpoint_name, monitor='val_loss', verbose = 1, save_best_only = True, mode ='auto')
 callbacks_list = [checkpoint]
 
 #Train the model
-NN_model.fit(train, target, epochs=200, batch_size=1, validation_split = 0.2, callbacks=callbacks_list)
+NN_model.fit(train, target, epochs=350, batch_size=1, validation_split = 0.2, callbacks=callbacks_list)
+
 
 # Load wights file of the best model :
-wights_file = 'Weights-OK-15.82156.hdf5' # choose the best checkpoint 
+wights_file = 'Weights_14.6.hdf5' # choose the best checkpoint 
 NN_model.load_weights(wights_file) # load it
 NN_model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_absolute_error'])
 
@@ -155,4 +157,4 @@ def make_submission(prediction, sub_name):
   print('A submission file has been made')
 
 predictions = NN_model.predict(test)
-make_submission(predictions[:,0],'submission(NN).csv')
+make_submission(predictions[:,0],'Prediction result.csv')
