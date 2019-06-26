f11c3719cf9a758693ae97195705f766ff8aa659
Mrpsousa <urameshi.uba@gmail.com>
2019-06-26 16:42:47 -0300

    inicio dos testes

--- rede_keras/rede01.py
+++ rede_keras/rede01.py
@@ -122,13 +122,12 @@ Use ‘linear ’as the activation function for the output layer'''
 NN_model = Sequential()
 
 # The Input Layer :
-NN_model.add(Dense(15, kernel_initializer='normal',input_dim = train.shape[1], activation='relu'))
+NN_model.add(Dense(11, kernel_initializer='normal',input_dim = train.shape[1], activation='relu'))
 
 # The Hidden Layers :
-NN_model.add(Dense(30, kernel_initializer='normal',activation='relu'))
-NN_model.add(Dense(30, kernel_initializer='normal',activation='relu'))
-NN_model.add(Dense(30, kernel_initializer='normal',activation='relu'))
-NN_model.add(Dense(30, kernel_initializer='normal',activation='relu'))
+NN_model.add(Dense(22, kernel_initializer='normal',activation='relu'))
+NN_model.add(Dense(22, kernel_initializer='normal',activation='relu'))
+NN_model.add(Dense(22, kernel_initializer='normal',activation='relu'))
 
 # The Output Layer :
 NN_model.add(Dense(1, kernel_initializer='normal',activation='linear'))
@@ -144,10 +143,10 @@ checkpoint = ModelCheckpoint(checkpoint_name, monitor='val_loss', verbose = 1, s
 callbacks_list = [checkpoint]
 
 #Train the model
-NN_model.fit(train, target, epochs=800, batch_size=3, validation_split = 0.2, callbacks=callbacks_list)
+NN_model.fit(train, target, epochs=1200, batch_size=3, validation_split = 0.2, callbacks=callbacks_list)
 
 # Load wights file of the best model :
-wights_file = 'Weights-343--847.30898.hdf5' # choose the best checkpoint 
+wights_file = 'Weights-1166--730.58780.hdf5' # choose the best checkpoint 
 NN_model.load_weights(wights_file) # load it
 NN_model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_absolute_error'])
 
@@ -157,4 +156,4 @@ def make_submission(prediction, sub_name):
   print('A submission file has been made')
 
 predictions = NN_model.predict(test)
-make_submission(predictions[:,0],'Prediction result.txt')
+make_submission(predictions[:,0],'Prediction result')
